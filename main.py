5deaa474e3aad49017033dd9448ef54fdcd55df8
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2025-01-12 00:18:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import re
 import time
 from datetime import timedelta
 from typing import Set
-
+from pyrogram.types import CallbackQuery
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
