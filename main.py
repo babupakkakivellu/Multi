f8f073e28677aaf20d47e473fa64fec0461766ab
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2025-01-13 11:51:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Client(
     "stream_remover_bot",
     api_id="16501053",
     api_hash="d8c9b01c863dabacc484c2c06cdd0f6e",
-    bot_token="8125717355:AAGEqXec28WfZ5V_wb4bkKoSyTt_slw6x2I"
+    bot_token="6738287955:AAE5lXdu_kbQevdyImUIJ84CTwwNhELjHK4"
 )
 
 # Store user data
