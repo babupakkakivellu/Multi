6bb824981c32ca596b5c204c3157794e60eb3722
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2025-01-15 14:21:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ BOT_TOKEN = "8125717355:AAGEqXec28WfZ5V_wb4bkKoSyTt_slw6x2I"
 # Initialize your Telegram bot
 app = Client(
     "video_compression_bot",
-    api_id="YOUR_API_ID",
-    api_hash="YOUR_API_HASH",
-    bot_token="YOUR_BOT_TOKEN"
+    api_id=API_ID
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
 
 # Store user states and settings
